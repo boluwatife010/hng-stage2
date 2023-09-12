52e47324d6a6e2da0bbe90263ca18f64d58b7fcc
boluwatife010 <temitopeboluojo@gmail.com>
2023-09-12 14:11:50 +0100

    commit 4 task 2

--- app.ts
+++ app.ts
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 app.use('/api', personRouter);
 app.listen(8083, async () => {
     console.log('server is connected at port 8083');
-    await mongoose.connect('mongodb://127.0.0.1/hng-personDetails');
-    console.log('Connected to mongoDB');
+    await mongoose.connect('mongodb://127.0.0.1/hng-personDetails', {})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 });
 export{};
